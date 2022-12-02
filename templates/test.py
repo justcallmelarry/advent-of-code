693809502a63f122403a7db59e16afbfc635fbce
Lauri Vuorela <lauri.vuorela@gmail.com>
2022-12-02 16:30:08 +0100

    add skips if tests are not updated

--- templates/test.py
+++ templates/test.py
@@ -17,5 +17,7 @@ yearday = re.sub("[^0-9]", "", str(__file__))
     ],
 )
 def test_result(part_name: Literal["a", "b"], expected: str, sample: bool) -> None:
+    if expected == "CHANGEME":
+        pytest.skip()
     mod = importlib.import_module(f"{yearday[:4]}.{str(yearday[-2:]).zfill(2)}.{part_name}")
     assert mod.main(sample) == expected
