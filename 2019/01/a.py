536ac802f39be6baa5b2fd36d5aaf5a71d11f3f1
Lauri Vuorela <lauri.vuorela@gmail.com>
2022-12-02 12:26:08 +0100

    add day1 2019 as test for tooling

---
+++ 2019/01/a.py
@@ -0,0 +1,23 @@
+import collections
+import os
+import sys
+
+import injection
+import utils
+
+
+def get_fuel(x: int) -> int:
+    return int(x / 3) - 2
+
+
+@injection.input_injection
+def main(_input: str, sample_input: bool = False) -> str:
+    result: int = 0
+
+    result = sum(get_fuel(x) for x in utils.ints(_input))
+
+    return str(result)
+
+
+if __name__ == "__main__":
+    print(main(True if "--sample" in sys.argv else False))
---
+++ 2019/01/b.py
@@ -0,0 +1,29 @@
+import sys
+
+import injection
+import utils
+
+
+def get_fuel(x: int) -> int:
+    fuel = int(x / 3) - 2
+    if fuel < 0:
+        return 0
+    return fuel
+
+
+@injection.input_injection
+def main(_input: str, sample_input: bool = False) -> str:
+    result: int = 0
+
+    for x in utils.ints(_input):
+        fuel = get_fuel(x)
+        result += fuel
+        while fuel:
+            fuel = get_fuel(fuel)
+            result += fuel
+
+    return str(result)
+
+
+if __name__ == "__main__":
+    print(main(True if "--sample" in sys.argv else False))
