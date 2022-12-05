9c53ba7e0a426cb5ee587088006b5ebe590514b2
Lauri Vuorela <lauri.vuorela@gmail.com>
2022-12-05 10:19:18 +0100

    refactor parsing a bit

--- 2022/05/a.py
+++ 2022/05/a.py
@@ -1,6 +1,6 @@
-import collections
 import re
 import sys
+from itertools import zip_longest
 
 import utils
 from injection import input_injection
@@ -12,17 +12,10 @@ def main(_input: str, sample_input: bool = False) -> str:
     base_rows = base.splitlines()
     columns = {i: "" for i in utils.positive_ints(base_rows.pop(-1))}
 
+    base_rows.reverse()
     for row in base_rows:
-        step = 3
-        current_col = 1
-        while len(row) >= step:
-            crate = row[:3]
-            columns[current_col] = re.sub("[^A-Z]", "", crate) + columns[current_col]
-
-            row = row[3:]
-            if len(row) >= 4:
-                row = row[1:]
-            current_col += 1
+        for i, box in enumerate(row[1::4], start=1):
+            columns[i] += re.sub("[^A-Z]", "", box)
 
     for line in directions.splitlines():
         cmd = utils.positive_ints(line)
@@ -36,6 +29,35 @@ def main(_input: str, sample_input: bool = False) -> str:
             columns[from_] = columns[from_][:-1]
 
     return str("".join(x[-1] for x in columns.values() if x))
+
+
+def print_columns(columns: dict) -> None:
+    """
+    not really used, but fun mostly for visualization
+    """
+    cols = []
+    base = " ".join(str(c) for c in columns.keys())
+    for t in zip_longest(*columns.values(), fillvalue=" "):
+        cols.append(" ".join(t))
+    cols.reverse()
+    stacks = "\n".join(cols)
+    print(f"{stacks}\n{base}\n")
+
+
+def original_stack_parser(base_rows: list) -> None:
+    columns = {i: "" for i in utils.positive_ints(base_rows.pop(-1))}
+
+    for row in base_rows:
+        step = 3
+        current_col = 1
+        while len(row) >= step:
+            crate = row[:3]
+            columns[current_col] = re.sub("[^A-Z]", "", crate) + columns[current_col]
+
+            row = row[3:]
+            if len(row) >= 4:
+                row = row[1:]
+            current_col += 1
 
 
 if __name__ == "__main__":
