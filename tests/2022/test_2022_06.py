8b8e4f9f4eeccccbc7a19cb73d831c41bb630992
Lauri Vuorela <lauri.vuorela@gmail.com>
2022-12-06 12:08:59 +0100

    make it possible to provide input if wanted

--- tests/2022/test_2022_06.py
+++ tests/2022/test_2022_06.py
@@ -8,16 +8,25 @@ yearday = re.sub("[^0-9]", "", str(__file__))
 
 
 @pytest.mark.parametrize(
-    ("part_name", "expected", "sample"),
+    ("part_name", "expected", "sample", "provided_input"),
     [
-        ("a", "7", True),
-        ("b", "19", True),
-        ("a", "1647", False),
-        ("b", "2447", False),
+        ("a", "7", True, ""),
+        ("a", "5", True, "bvwbjplbgvbhsrlpgdmjqwftvncz"),
+        ("a", "6", True, "nppdvjthqldpwncqszvftbrmjlhg"),
+        ("a", "10", True, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+        ("a", "11", True, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+        ("b", "19", True, ""),
+        ("b", "19", True, "mjqjpqmgbljsphdztnvjfqwrcgsmlb"),
+        ("b", "23", True, "bvwbjplbgvbhsrlpgdmjqwftvncz"),
+        ("b", "23", True, "nppdvjthqldpwncqszvftbrmjlhg"),
+        ("b", "29", True, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+        ("b", "26", True, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+        ("a", "1647", False, ""),
+        ("b", "2447", False, ""),
     ],
 )
-def test_result(part_name: Literal["a", "b"], expected: str, sample: bool) -> None:
+def test_result(part_name: Literal["a", "b"], expected: str, sample: bool, provided_input: str) -> None:
     if expected == "CHANGEME":
         pytest.skip()
     mod = importlib.import_module(f"{yearday[:4]}.{str(yearday[-2:]).zfill(2)}.{part_name}")
-    assert mod.main(sample) == expected
+    assert mod.main(sample=sample, provided_input=provided_input) == expected
--- tools/injection.py
+++ tools/injection.py
@@ -5,11 +5,13 @@ import aoc
 
 
 def input_injection(func: Callable) -> Callable:
-    def wrapper(sample: bool) -> str:
+    def wrapper(sample: bool, provided_input: str = "") -> str:
         func_mod_path = inspect.getfile(func)
         _, year, day = func_mod_path.rsplit("/", 3)[:3]
 
-        if sample:
+        if provided_input:
+            _input = provided_input
+        elif sample:
             _input = aoc.get_sample(day=day, year=int(year))
         else:
             _input = aoc.get_actual(day=int(day), year=int(year))
