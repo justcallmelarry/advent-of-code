5b9a146c99608d5ead10d0d7fa0d738efd9b4bef
Lauri Vuorela <lauri.vuorela@gmail.com>
2022-12-07 11:12:17 +0100

    completely different file names

--- tests/2022/test_2022_07.py
+++ tests/2022/test_2022_07.py
@@ -32,32 +32,32 @@ $ ls
 
 own_sample="""$ cd /
 $ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
+dir b
+14848514 c.txt
+8504156 f.dat
 dir e
-$ cd a
+dir f
+$ cd b
 $ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
+dir f
+29116 g
+2557 h
+62596 i.lst
+$ cd f
 $ ls
-584 i
+584 j
 $ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
 $ cd ..
 $ cd e
 $ ls
-100001 i"""
+4060174 k
+8033020 e.log
+5626152 e.ext
+7214296 l
+$ cd ..
+$ cd f
+$ ls
+100001 j"""
 
 
 @pytest.mark.parametrize(
