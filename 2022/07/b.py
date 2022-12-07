abc07922b5200f1d27ee0b5da7a4958f3084ad4f
Lauri Vuorela <lauri.vuorela@gmail.com>
2022-12-07 11:06:18 +0100

    accidentally screwed up part b

--- 2022/07/b.py
+++ 2022/07/b.py
@@ -6,7 +6,7 @@ import utils
 from injection import input_injection
 
 
-def get_tree(_input: str) -> dict[Path, int]:
+def get_data(_input: str) -> tuple[int, dict[Path, int]]:
     tree: dict[Path, int] = collections.defaultdict(int)
     current_path = Path()
 
@@ -33,19 +33,18 @@ def get_tree(_input: str) -> dict[Path, int]:
             key = key.parent
             new_tree[key] += value
 
-    return new_tree
+    return sum(tree.values()), new_tree
 
 
 @input_injection
 def main(_input: str) -> str:
     result: int = 0
 
-    tree = get_tree(_input)
+    space_used, tree = get_data(_input)
 
     total_space = 70000000
     need_space = 30000000
 
-    space_used = sum(tree.values())
     free_up = need_space - (total_space - space_used)
 
     possible_dirs = [v for v in tree.values() if v >= free_up]
--- tests/2022/test_2022_07.py
+++ tests/2022/test_2022_07.py
@@ -30,12 +30,43 @@ $ ls
 5626152 d.ext
 7214296 k"""
 
+own_sample="""$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+dir e
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+$ cd ..
+$ cd e
+$ ls
+100001 i"""
+
 
 @pytest.mark.parametrize(
     ("part_name", "expected", "provided_input"),
     [
         ("a", "95437", sample),
         ("b", "24933642", sample),
+        ("a", "95437", own_sample),
+        ("b", "24933642", own_sample),
         ("a", "1667443", ""),
         ("b", "8998590", ""),
     ],
