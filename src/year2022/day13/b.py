deed54a42d6ba46b2bf2b5146581df13a46b70d6
Lauri Vuorela <lauri.vuorela@gmail.com>
2022-12-13 10:53:01 +0100

    do not bubble sort

--- src/year2022/day13/b.py
+++ src/year2022/day13/b.py
@@ -1,27 +1,25 @@
 import ast
-import functools
 
 from injection import input_injection
 from year2022.day13.a import signal_order_check
 
 
-def compare_sorter(a: list | int, b: list | int) -> int:
-    if signal_order_check(a, b):
-        return -1
-    return 1
-
-
 @input_injection
 def main(_input: str) -> str:
+    """
+    my original solution used bubble sort, but we only need to know how many signals the
+    packet actually compares favourably against, letting us do less comparisons
+    """
     result: int = 0
 
-    packet_a = [[2]]
-    packet_b = [[6]]
+    signals = [ast.literal_eval(x) for x in _input.split()]
 
-    signals = [ast.literal_eval(x) for x in _input.split()] + [packet_a, packet_b]
+    # add one because index starts at 1
+    position_1 = sum(1 for signal in signals if signal_order_check(signal, [[2]])) + 1
+    # add two since first packet is also added already
+    position_2 = sum(1 for signal in signals if signal_order_check(signal, [[6]])) + 2
 
-    signals.sort(key=functools.cmp_to_key(compare_sorter))
-    result = (signals.index(packet_a) + 1) * (signals.index(packet_b) + 1)
+    result = position_1 * position_2
 
     return str(result)
 
