64e494d9d947b959f3955c7bea7ed55eb84d8ae4
Lauri Vuorela <lauri.vuorela@gmail.com>
2022-12-16 23:04:53 +0100

    add 2022-16 part 1

---
+++ src/year2022/day16/a.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+
+from collections import deque
+from dataclasses import dataclass
+
+from injection import input_injection
+
+sample = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II"""
+
+
+@dataclass(order=True)
+class Valve:
+    name: str
+    flow_rate: int
+    tunnels: list[Valve]
+
+    def go_to(self, name: str) -> Valve:
+        for v in self.tunnels:
+            if v.name == name:
+                return v
+
+        raise Exception("No such tunnel")
+
+    def __repr__(self) -> str:
+        return f"Valve({self.name} flow_rate={self.flow_rate}"
+
+
+@dataclass
+class Volcano:
+    current_position: Valve
+    valves: dict[str, Valve]
+    opened: list[str]
+    path: deque[str]
+    total: int = 0
+    current_flow: int = 0
+    minutes_lapsed: int = 1
+
+
+def get_valves(_input: str) -> dict[str, Valve]:
+    valves: dict[str, Valve] = {}
+
+    for line in _input.splitlines():
+        x = line.split()
+        name = x[1]
+        flow_rate = int(x[4].split("=")[-1].strip(";"))
+
+        if not name in valves:
+            valves[name] = Valve(name=name, flow_rate=0, tunnels=[])
+
+        valve = valves[name]
+        valve.flow_rate = flow_rate
+
+        for tunnel_name in x[9:]:
+            tunnel_name = tunnel_name.strip(",")
+            if tunnel_name not in valves:
+                valves[tunnel_name] = Valve(name=tunnel_name, flow_rate=0, tunnels=[])
+            tunnel_valve = valves[tunnel_name]
+            valve.tunnels.append(tunnel_valve)
+
+    return valves
+
+
+@dataclass
+class QeueuItem:
+    valve: Valve
+    path: deque[str]
+
+
+__CACHE: dict[tuple[str, str], deque[str]] = {}
+
+
+def get_shortest_path(start: Valve, target: Valve) -> deque[str]:
+    q = deque([QeueuItem(valve=start, path=deque([]))])
+    seen: set[str] = set([])
+
+    while q:
+        cur = q.popleft()
+        for t in cur.valve.tunnels:
+            if t.name in seen:
+                continue
+
+            new_path = cur.path.copy()
+            new_path.append(t.name)
+
+            if t == target:
+                if (start.name, target.name) in __CACHE:
+                    return __CACHE[(start.name, target.name)].copy()
+                __CACHE[(start.name, target.name)] = new_path
+                return new_path.copy()
+
+            seen.add(t.name)
+            q.append(QeueuItem(valve=t, path=new_path))
+
+    raise Exception("Not found")
+
+
+class NextVolcano(Exception):
+    pass
+
+
+def volcano_game(original_state: Volcano) -> int:
+    volcanoes: deque[Volcano] = deque([original_state])
+    biggest = 0
+
+    possible_turnons = [t for t in original_state.valves.values() if t.flow_rate != 0]
+
+    while volcanoes:
+        volcano = volcanoes.pop()
+        current_position = volcano.current_position
+        try:
+            for minute in range(volcano.minutes_lapsed, 31):
+                volcano.total += volcano.current_flow
+                # continue on your way
+                if volcano.path:
+                    current_position = current_position.go_to(volcano.path.popleft())
+                    continue
+
+                # open a valve
+                elif current_position.flow_rate != 0 and current_position.name not in volcano.opened:
+                    volcano.current_flow += current_position.flow_rate
+                    volcano.opened.append(current_position.name)
+                    continue
+
+                # otherwise explore options
+                for v in possible_turnons:
+                    if v.name in volcano.opened or v == current_position:
+                        continue
+
+                    shortest = get_shortest_path(current_position, v)
+                    volcanoes.append(
+                        Volcano(
+                            current_position=current_position.go_to(shortest.popleft()),
+                            valves=volcano.valves,
+                            opened=volcano.opened.copy(),
+                            path=shortest,
+                            total=volcano.total,
+                            current_flow=volcano.current_flow,
+                            minutes_lapsed=minute + 1,
+                        )
+                    )
+                if len(volcano.opened) != len(possible_turnons):
+                    raise NextVolcano
+        except NextVolcano:
+            pass
+        if volcano.total > biggest:
+            biggest = volcano.total
+
+    return biggest
+
+
+@input_injection
+def main(_input: str) -> str:
+    valves = get_valves(_input)
+    result = 0
+
+    aa = valves["AA"]
+    volcano = Volcano(current_position=aa, valves=valves, opened=[], path=deque())
+
+    result = volcano_game(volcano)
+
+    return str(result)
+
+
+if __name__ == "__main__":
+    print(main(provided_input=sample))
---
+++ tests/2022/test_2022_16.py
@@ -0,0 +1,37 @@
+import importlib
+import re
+from typing import Literal
+
+import pytest
+
+from infra import get_mod_path
+
+yearday = re.sub("[^0-9]", "", str(__file__))
+
+sample = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II"""
+
+
+@pytest.mark.parametrize(
+    ("part_name", "expected", "provided_input"),
+    [
+        ("a", "1651", sample),
+        ("b", "CHANGEME", sample),
+        ("a", "1828", ""),
+        ("b", "CHANGEME", ""),
+    ],
+)
+def test_result(part_name: Literal["a", "b"], expected: str, provided_input: str) -> None:
+    if expected == "CHANGEME":
+        pytest.skip()
+    mod_path = get_mod_path(int(yearday[:4]), int(yearday[-2:]), part_name)
+    mod = importlib.import_module(mod_path)
+    assert mod.main(provided_input=provided_input) == expected
